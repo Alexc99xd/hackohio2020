aa176c8ec6af2f1d91dc5e073ca0f127a765bf98
Alex Chan <chan.730@osu.edu>
2020-11-14 00:04:54 -0500

    progress bar

--- public/scripts/community.js
+++ public/scripts/community.js
@@ -24,4 +24,63 @@ function checkStats(){
     }).catch(function(error) {
         console.log("Error getting document:", error);
     });
+
+    updateProgressBar();
+}
+
+function updateProgressBar(){
+
+    var docRef = db.collection("stats").doc("sticker");
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            var docData = doc.data();
+            var count = docData.count;
+            const stickerGoal = 6969;
+            const percent = count/stickerGoal;
+
+            document.getElementById('progress-details').innerHTML = `We have received ${(percent * 100).toFixed(2)}% of our goal of ${stickerGoal} stickers`;
+
+            var bar = new ProgressBar.SemiCircle('#progress-container', {
+                strokeWidth: 6,
+                easing: 'easeInOut',
+                color: '#9600ff',
+                trailColor: '#ea3001',
+                trailWidth: 1,
+                duration: 1400,
+                text: {
+                    value: '',
+                    alignToBottom: true
+                },
+                from: {color: '#9600ff'},
+                to: {color: '#ea3001'},
+                // Set default step function for all animate calls
+                step: (state, bar) => {
+                    bar.path.setAttribute('stroke', state.color);
+                    var value = Math.round(bar.value() * 100);
+                    if (value === 0) {
+                    bar.setText('');
+                    } else {
+                    bar.setText(count + " stickers!");
+                    }
+            
+                    bar.text.style.color = state.color;
+                    //document.getElementById('progress-details').css("color", state.color);
+                }
+                });
+                bar.text.style.fontSize = '4rem';
+                if(percent >= 1){
+                    bar.animate(1);
+                } else {
+                    bar.animate(percent);
+                }
+        }
+    });
+
+    
+}
+
+
+function animate(){
+    requestAnimationFrame(animate);
+    renderer.render(stage);
 }
