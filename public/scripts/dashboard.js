b09ff774f5f923abad4d29703a8d48996a9ec6da
Thrump <vonchude@gmail.com>
2020-11-15 02:45:25 -0500

    Changed styling on dashboard

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,14 +1,13 @@
-
 var db = firebase.firestore();
 var loaded = false;
 
-function startCircle(count){
+function startCircle(count) {
     // progressbar.js@1.0.0 version is used
     // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
     const stickerMax = 10; //change this
-    const percent = count/stickerMax;
+    const percent = count / stickerMax;
     var bar = new ProgressBar.Circle(container, {
-        color: '#9600ff',
+        color: '#000000',
         // This has to be the same size as the maximum width to
         // prevent clipping
         strokeWidth: 4,
@@ -16,43 +15,43 @@ function startCircle(count){
         easing: 'easeInOut',
         duration: 2400,
         text: {
-        autoStyleContainer: false
+            autoStyleContainer: false
         },
-        from: { color: '#9600ff', width: 1 },
+        from: { color: '#000000', width: 1 },
         to: { color: '#333', width: 4 },
         // Set default step function for all animate calls
         step: function(state, circle) {
-        circle.path.setAttribute('stroke', state.color);
-        circle.path.setAttribute('stroke-width', state.width);
-    
-        var value = Math.round(circle.value() * 100);
-        if (value === 0) {
-            circle.setText('');
-        } else {
-            circle.setText(value + "%");
-        }
-    
+            circle.path.setAttribute('stroke', state.color);
+            circle.path.setAttribute('stroke-width', state.width);
+
+            var value = Math.round(circle.value() * 100);
+            if (value === 0) {
+                circle.setText('');
+            } else {
+                circle.setText(value + "%");
+            }
+
         }
     });
     bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
     bar.text.style.fontSize = '2rem';
-    
+
     // bar.animate(1.0);  // Number from 0.0 to 1.0
-    if(percent >= 1){
+    if (percent >= 1) {
         bar.animate(1);
     } else {
         bar.animate(percent);
     }
 }
-async function loadStickers(){
-    if(!loaded){
+async function loadStickers() {
+    if (!loaded) {
         loaded = true;
         var docRef = db.collection("users").doc(firebase.auth().currentUser.uid);
         docRef.get().then(function(doc) {
             if (doc.exists) {
-                for(let i = 1; i < 5; i++){
+                for (let i = 1; i < 5; i++) {
                     var str = "sticker_" + i;
-                    if(doc.data()[str] != null){
+                    if (doc.data()[str] != null) {
                         //then get code
                         var code = doc.data()[str];
 
@@ -62,9 +61,14 @@ async function loadStickers(){
 
                     }
                 }
-                
-                
+
+
                 document.getElementById("sticker_title").innerHTML = "Number of stickers you have: " + doc.data().sticker_count + " stickers!";
+
+                Array.from(document.getElementsByClassName("loadSticker")).forEach(element => {
+                    element.style.display = "block";
+                })
+                document.getElementById("bruh").style.display = "none";
                 startCircle(doc.data().sticker_count);
             } else {
 
@@ -76,18 +80,18 @@ async function loadStickers(){
     }
 }
 
-function getPlace(code, str){
-    
+function getPlace(code, str) {
+
     var docRef2 = db.collection("stickers").doc(code);
-    
+
     docRef2.get().then(function(doc) {
         console.log(doc.data());
         if (doc.exists) {
-            document.getElementById('stickerList').innerHTML += ('<li>'+ str + " from " + doc.data().place +'</li>');
+            document.getElementById('stickerList').innerHTML += ('<li class="list-group-item">' + str + " from " + doc.data().place + '</li>');
             //return doc.data().place;
         }
     }).catch(function(error) {
         console.log("Error getting document:", error);
     });
-    
+
 }
